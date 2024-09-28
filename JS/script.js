769735a5efc0dfb39e6b653fa1eed211e3ba16a4
Leo Fantuz Chrobot <leof150105@gmail.com>
2024-09-29 01:53:30 +0200

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,320 @@
+let projectImage = document.getElementById("projectImage");
+let projectTitle = document.getElementById("project-title");
+let projectDescription = document.getElementById("project-description");
+
+
+let Itinéraires_button = document.getElementById("Itinéraires");
+let SpaceOfQuestion_button = document.getElementById("SpaceOfQuestion");
+let Covoiturage_button = document.getElementById("Covoiturage");
+let Banko_button = document.getElementById("Banko");
+let Activities_button = document.getElementById("Activities");
+let BDD_button = document.getElementById("BDD");
+let FindUrWay_button = document.getElementById("FindUrWay");
+let SpaceClicker_button = document.getElementById("SpaceClicker");
+
+
+let pageWeb = "https://gitlab.com/leo9440022/SAE/-/tree/main/S2/SAE_2_1_Algo/Projet?ref_type=heads";
+
+
+
+function Itinéraires(){
+    Itinéraires_button.style.backgroundColor = "#576C54";
+    SpaceOfQuestion_button.style.backgroundColor = "transparent";
+    Covoiturage_button.style.backgroundColor = "transparent";
+    Banko_button.style.backgroundColor = "transparent";
+    Activities_button.style.backgroundColor = "transparent";
+    BDD_button.style.backgroundColor = "transparent";
+    FindUrWay_button.style.backgroundColor = "transparent";
+    SpaceClicker_button.style.backgroundColor = "transparent";
+
+
+    Itinéraires_button.style.borderColor = "#CFFFB8";
+    SpaceOfQuestion_button.style.borderColor = "#F6F6F6";
+    Covoiturage_button.style.borderColor = "#F6F6F6";
+    Banko_button.style.borderColor = "#F6F6F6";
+    Activities_button.style.borderColor = "#F6F6F6";
+    BDD_button.style.borderColor = "#F6F6F6";
+    FindUrWay_button.style.borderColor = "#F6F6F6";
+    SpaceClicker_button.style.borderColor = "#F6F6F6";
+
+    Itinéraires_button.style.boxShadow = "0px 6px 5px rgb(37, 37, 37)";
+    SpaceOfQuestion_button.style.boxShadow = "none";
+    Covoiturage_button.style.boxShadow = "none";
+    Banko_button.style.boxShadow = "none";
+    Activities_button.style.boxShadow = "none";
+    BDD_button.style.boxShadow = "none";
+    FindUrWay_button.style.boxShadow = "none";
+    SpaceClicker_button.style.boxShadow = "none";
+
+    pageWeb = "https://gitlab.com/leo9440022/SAE/-/tree/main/S2/SAE_2_1_Algo/Projet?ref_type=heads";
+    projectImage.src = "IMG/itineraires.png";
+    projectTitle.innerText = "Itinéraires";
+    projectDescription.innerText = "Le projet Itinéraire a pour objectif d’apprendre à manipuler une librairie donnée afin d’élaborer une application permettant de calculer le chemin le plus court d'un point A à un point B.";
+}
+
+function SpaceOfQuestion(){
+    Itinéraires_button.style.backgroundColor = "transparent";
+    SpaceOfQuestion_button.style.backgroundColor = "#576C54";
+    Covoiturage_button.style.backgroundColor = "transparent";
+    Banko_button.style.backgroundColor = "transparent";
+    Activities_button.style.backgroundColor = "transparent";
+    BDD_button.style.backgroundColor = "transparent";
+    FindUrWay_button.style.backgroundColor = "transparent";
+    SpaceClicker_button.style.backgroundColor = "transparent";
+
+
+    Itinéraires_button.style.borderColor = "#F6F6F6";
+    SpaceOfQuestion_button.style.borderColor = "#CFFFB8";
+    Covoiturage_button.style.borderColor = "#F6F6F6";
+    Banko_button.style.borderColor = "#F6F6F6";
+    Activities_button.style.borderColor = "#F6F6F6";
+    BDD_button.style.borderColor = "#F6F6F6";
+    FindUrWay_button.style.borderColor = "#F6F6F6";
+    SpaceClicker_button.style.borderColor = "#F6F6F6";
+
+    Itinéraires_button.style.boxShadow = "none";
+    SpaceOfQuestion_button.style.boxShadow = "0px 6px 5px rgb(37, 37, 37)";
+    Covoiturage_button.style.boxShadow = "none";
+    Banko_button.style.boxShadow = "none";
+    Activities_button.style.boxShadow = "none";
+    BDD_button.style.boxShadow = "none";
+    FindUrWay_button.style.boxShadow = "none";
+    SpaceClicker_button.style.boxShadow = "none";
+
+    pageWeb = "https://gitlab.com/leo9440022/SAE/-/tree/main/S1/SAE%201_2%20comparaison%20d'algo/SpaceOfQuestions?ref_type=heads";
+    projectImage.src = "IMG/Space_Of_Questions.png";
+    projectTitle.innerText = "Space Of Questions";
+    projectDescription.innerText = "'Space Of Questions' est un jeu de questions ludo-pédagogique destiné à des élèves de primaire, leur permettant ainsi de réviser leurs leçons de manière amusante en répondant à des questions de cours. Ce jeu à été élaboré sans interface graphique.";
+}
+
+function Covoiturage(){
+    Itinéraires_button.style.backgroundColor = "transparent";
+    SpaceOfQuestion_button.style.backgroundColor = "transparent";
+    Covoiturage_button.style.backgroundColor = "#576C54";
+    Banko_button.style.backgroundColor = "transparent";
+    Activities_button.style.backgroundColor = "transparent";
+    BDD_button.style.backgroundColor = "transparent";
+    FindUrWay_button.style.backgroundColor = "transparent";
+    SpaceClicker_button.style.backgroundColor = "transparent";
+
+
+    Itinéraires_button.style.borderColor = "#F6F6F6";
+    SpaceOfQuestion_button.style.borderColor = "#F6F6F6";
+    Covoiturage_button.style.borderColor = "#CFFFB8";
+    Banko_button.style.borderColor = "#F6F6F6";
+    Activities_button.style.borderColor = "#F6F6F6";
+    BDD_button.style.borderColor = "#F6F6F6";
+    FindUrWay_button.style.borderColor = "#F6F6F6";
+    SpaceClicker_button.style.borderColor = "#F6F6F6";
+
+    Itinéraires_button.style.boxShadow = "none";
+    SpaceOfQuestion_button.style.boxShadow = "none";
+    Covoiturage_button.style.boxShadow = "0px 6px 5px rgb(37, 37, 37)";
+    Banko_button.style.boxShadow = "none";
+    Activities_button.style.boxShadow = "none";
+    BDD_button.style.boxShadow = "none";
+    FindUrWay_button.style.boxShadow = "none";
+    SpaceClicker_button.style.boxShadow = "none";
+
+    pageWeb = "https://gitlab.com/leo9440022/SAE/-/tree/main/S1/SAE%201.5_1.6%20RdB/Fantuz-Chrobot,%20Devos,%20Okubo,%20Henneron%20site%20RdB%20(Blizzard%20Entertainment)?ref_type=heads";
+    projectImage.src = "IMG/Covoiturage.png";
+    projectTitle.innerText = "Covoiturage";
+    projectDescription.innerText = "Covoiturage est un service de covoiturage et de location de véhicules à destination des employés de l’entreprise Blizzard Entertainment (il s'agit d'une mise en situation). Ces services sont présentés sont la forme d’une nouvelle page web présente pour les employés de manière totalement privée.";
+}
+
+function Banko(){
+    Itinéraires_button.style.backgroundColor = "transparent";
+    SpaceOfQuestion_button.style.backgroundColor = "transparent";
+    Covoiturage_button.style.backgroundColor = "transparent";
+    Banko_button.style.backgroundColor = "#576C54";
+    Activities_button.style.backgroundColor = "transparent";
+    BDD_button.style.backgroundColor = "transparent";
+    FindUrWay_button.style.backgroundColor = "transparent";
+    SpaceClicker_button.style.backgroundColor = "transparent";
+
+
+    Itinéraires_button.style.borderColor = "#F6F6F6";
+    SpaceOfQuestion_button.style.borderColor = "#F6F6F6";
+    Covoiturage_button.style.borderColor = "#F6F6F6";
+    Banko_button.style.borderColor = "#CFFFB8";
+    Activities_button.style.borderColor = "#F6F6F6";
+    BDD_button.style.borderColor = "#F6F6F6";
+    FindUrWay_button.style.borderColor = "#F6F6F6";
+    SpaceClicker_button.style.borderColor = "#F6F6F6";
+
+    Itinéraires_button.style.boxShadow = "none";
+    SpaceOfQuestion_button.style.boxShadow = "none";
+    Covoiturage_button.style.boxShadow = "none";
+    Banko_button.style.boxShadow = "0px 6px 5px rgb(37, 37, 37)";
+    Activities_button.style.boxShadow = "none";
+    BDD_button.style.boxShadow = "none";
+    FindUrWay_button.style.boxShadow = "none";
+    SpaceClicker_button.style.boxShadow = "none";
+
+    pageWeb = "https://gitlab.com/leo9440022/sites-perso/-/tree/main/Banko?ref_type=heads";
+    projectImage.src = "IMG/Banko.png";
+    projectTitle.innerText = "Banko";
+    projectDescription.innerText = "Banko est un site web en cours de développement. Il s'agit d'un site permettant de créer et stocker des achats fait récemment afin de garder une trace de ces derniers. Les données sont ainsi stocker en local grâce à javascript.";
+}
+
+function Activities(){
+    Itinéraires_button.style.backgroundColor = "transparent";
+    SpaceOfQuestion_button.style.backgroundColor = "transparent";
+    Covoiturage_button.style.backgroundColor = "transparent";
+    Banko_button.style.backgroundColor = "transparent";
+    Activities_button.style.backgroundColor = "#576C54";
+    BDD_button.style.backgroundColor = "transparent";
+    FindUrWay_button.style.backgroundColor = "transparent";
+    SpaceClicker_button.style.backgroundColor = "transparent";
+
+
+    Itinéraires_button.style.borderColor = "#F6F6F6";
+    SpaceOfQuestion_button.style.borderColor = "#F6F6F6";
+    Covoiturage_button.style.borderColor = "#F6F6F6";
+    Banko_button.style.borderColor = "#F6F6F6";
+    Activities_button.style.borderColor = "#CFFFB8";
+    BDD_button.style.borderColor = "#F6F6F6";
+    FindUrWay_button.style.borderColor = "#F6F6F6";
+    SpaceClicker_button.style.borderColor = "#F6F6F6";
+
+    Itinéraires_button.style.boxShadow = "none";
+    SpaceOfQuestion_button.style.boxShadow = "none";
+    Covoiturage_button.style.boxShadow = "none";
+    Banko_button.style.boxShadow = "none";
+    Activities_button.style.boxShadow = "0px 6px 5px rgb(37, 37, 37)";
+    BDD_button.style.boxShadow = "none";
+    FindUrWay_button.style.boxShadow = "none";
+    SpaceClicker_button.style.boxShadow = "none";
+
+    pageWeb = "https://gitlab.com/leo9440022/java-projects/-/tree/main/site-alicia?ref_type=heads";
+    projectImage.src = "IMG/Activities.png";
+    projectTitle.innerText = "Activities";
+    projectDescription.innerText = "Activities est une application développer en javafx permettant de regrouper et afficher des activités présentes dans un fichier CSV chargé pour l'occasion. Cette application à était élaborée à des fins personnelles (Ceci expliquant le thème principal: pokémon).";
+}
+
+function BDD(){
+    Itinéraires_button.style.backgroundColor = "transparent";
+    SpaceOfQuestion_button.style.backgroundColor = "transparent";
+    Covoiturage_button.style.backgroundColor = "transparent";
+    Banko_button.style.backgroundColor = "transparent";
+    Activities_button.style.backgroundColor = "transparent";
+    BDD_button.style.backgroundColor = "#576C54";
+    FindUrWay_button.style.backgroundColor = "transparent";
+    SpaceClicker_button.style.backgroundColor = "transparent";
+
+
+    Itinéraires_button.style.borderColor = "#F6F6F6";
+    SpaceOfQuestion_button.style.borderColor = "#F6F6F6";
+    Covoiturage_button.style.borderColor = "#F6F6F6";
+    Banko_button.style.borderColor = "#F6F6F6";
+    Activities_button.style.borderColor = "#F6F6F6";
+    BDD_button.style.borderColor = "#CFFFB8";
+    FindUrWay_button.style.borderColor = "#F6F6F6";
+    SpaceClicker_button.style.borderColor = "#F6F6F6";
+
+    Itinéraires_button.style.boxShadow = "none";
+    SpaceOfQuestion_button.style.boxShadow = "none";
+    Covoiturage_button.style.boxShadow = "none";
+    Banko_button.style.boxShadow = "none";
+    Activities_button.style.boxShadow = "none";
+    BDD_button.style.boxShadow = "0px 6px 5px rgb(37, 37, 37)";
+    FindUrWay_button.style.boxShadow = "none";
+    SpaceClicker_button.style.boxShadow = "none";
+
+    pageWeb = "https://gitlab.com/leo9440022/SAE/-/tree/main/S2/SAE%202.4%20BDD?ref_type=heads";
+    projectImage.src = "IMG/exploitation_BDD.png";
+    projectTitle.innerText = "Exploitation BDD";
+    projectDescription.innerText = "En partant d’un fichier CSV (parcoursup) de 128 colonnes et 13 000 lignes, l’objectif fut l’importation de ce dernier sur postgresql, mais également la ventilation des données ainsi que l’exécution de certaines requêtes afin d’accéder à certaines informations de la base.";
+}
+
+function FindUrWay(){
+    Itinéraires_button.style.backgroundColor = "transparent";
+    SpaceOfQuestion_button.style.backgroundColor = "transparent";
+    Covoiturage_button.style.backgroundColor = "transparent";
+    Banko_button.style.backgroundColor = "transparent";
+    Activities_button.style.backgroundColor = "transparent";
+    BDD_button.style.backgroundColor = "transparent";
+    FindUrWay_button.style.backgroundColor = "#576C54";
+    SpaceClicker_button.style.backgroundColor = "transparent";
+
+
+    Itinéraires_button.style.borderColor = "#F6F6F6";
+    SpaceOfQuestion_button.style.borderColor = "#F6F6F6";
+    Covoiturage_button.style.borderColor = "#F6F6F6";
+    Banko_button.style.borderColor = "#F6F6F6";
+    Activities_button.style.borderColor = "#F6F6F6";
+    BDD_button.style.borderColor = "#F6F6F6";
+    FindUrWay_button.style.borderColor = "#CFFFB8";
+    SpaceClicker_button.style.borderColor = "#F6F6F6";
+
+    Itinéraires_button.style.boxShadow = "none";
+    SpaceOfQuestion_button.style.boxShadow = "none";
+    Covoiturage_button.style.boxShadow = "none";
+    Banko_button.style.boxShadow = "none";
+    Activities_button.style.boxShadow = "none";
+    BDD_button.style.boxShadow = "none";
+    FindUrWay_button.style.boxShadow = "0px 6px 5px rgb(37, 37, 37)";
+    SpaceClicker_button.style.boxShadow = "none";
+
+    pageWeb = "https://gitlab.com/leo9440022/sites-perso/-/tree/main/FindUrWay?ref_type=heads";
+    projectImage.src = "IMG/FindUrWay.png";
+    projectTitle.innerText = "Find Ur Way";
+    projectDescription.innerText = "Find Ur Way est un site web en cours de développement répertoriant de nombreux sites webs plus ou moins populaires permettant de découvrir ou trouver ces derniers en fonction de sa catégorie (Si l'on souhaite, par exemple, consulter certain article afin de se renseigner sur un sujet, il est possible de découvrir plein de site d'actualités pour y parvenir).";
+}
+
+
+function SpaceClicker(){
+    Itinéraires_button.style.backgroundColor = "transparent";
+    SpaceOfQuestion_button.style.backgroundColor = "transparent";
+    Covoiturage_button.style.backgroundColor = "transparent";
+    Banko_button.style.backgroundColor = "transparent";
+    Activities_button.style.backgroundColor = "transparent";
+    BDD_button.style.backgroundColor = "transparent";
+    FindUrWay_button.style.backgroundColor = "transparent";
+    SpaceClicker_button.style.backgroundColor = "#576C54";
+
+
+    Itinéraires_button.style.borderColor = "#F6F6F6";
+    SpaceOfQuestion_button.style.borderColor = "#F6F6F6";
+    Covoiturage_button.style.borderColor = "#F6F6F6";
+    Banko_button.style.borderColor = "#F6F6F6";
+    Activities_button.style.borderColor = "#F6F6F6";
+    BDD_button.style.borderColor = "#F6F6F6";
+    FindUrWay_button.style.borderColor = "#F6F6F6";
+    SpaceClicker_button.style.borderColor = "#CFFFB8";
+
+    Itinéraires_button.style.boxShadow = "none";
+    SpaceOfQuestion_button.style.boxShadow = "none";
+    Covoiturage_button.style.boxShadow = "none";
+    Banko_button.style.boxShadow = "none";
+    Activities_button.style.boxShadow = "none";
+    BDD_button.style.boxShadow = "none";
+    FindUrWay_button.style.boxShadow = "none";
+    SpaceClicker_button.style.boxShadow = "0px 6px 5px rgb(37, 37, 37)";
+
+    pageWeb = "https://gitlab.com/leo9440022/SAE/-/tree/main/S3/Mini-Projet?ref_type=heads";
+    projectImage.src = "IMG/SpaceClicker.png";
+    projectTitle.innerText = "Space Clicker";
+    projectDescription.innerText = "Space Clicker est un projet réalisé en 3 jours afin d'apprendre la méthode de travail agile scrum. Comme son nom l'indique, ce jeu est ainsi un clicker visant à propulser une fusée toujours plus haut dans l'espace afin de terminer le jeu. Ce dernier est uniquement disponible sans interface graphique, bien qu'un minimum illustré par des ascii arts.";
+}
+
+
+
+function openGitProject(){
+    window.location= pageWeb;
+}
+
+
+function openMail(){
+    window.location= "mailto:leof150105@gmail.com";
+}
+
+
+function openGitlab(){
+    window.location= "https://gitlab.com/leo9440022";
+}
+
+
+function openLinkedin(){
+    window.location= "https://www.linkedin.com/in/l%C3%A9o-fantuz-chrobot-9157b229b/";
+}
